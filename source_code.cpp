c904ff70818504cb231f9e1385f457cba8e99d6c
ngx_fus <75427876+nguyenphu-3204@users.noreply.github.com>
2023-06-19 16:06:03 +0700

    Update source_code.cpp

--- source_code.cpp
+++ source_code.cpp
@@ -1,33 +1,51 @@
 #include<bits/stdc++.h>
-#include "matrix.h"
+#include "squared_matrix.h"
 using namespace std;
 
+
+
+
 int main(){
-    matrix A, B;
-    A.set_matrix();
-    B.set_matrix();
-    A.print_matrix();
-    B.print_matrix();
-
-    cout << "C = A*3\n";
-    (A*3).print_matrix();
-
-    cout << "C = A+B\n";
-    (A+B).print_matrix();
-
-    cout << "C = A-B\n";
-    (A-B).print_matrix();
-
-    cout << "C = A*B\n";
-    (A*B).print_matrix();
-
-    cout << "C = B*A\n";
-    (B*A).print_matrix();
-
-    cout << "Bai 4\n";
-    A.f1_bai4();
-
-
+    squared_matrix a,b;
+    cin >> a;
+    matrix r = !a;
+    cout << r << endl;
     system("pause");
+    system("cls");
 return 0;
 }
+
+/*
+3
+2
+1 2      
+3 4
+8 5
+
+2 
+3
+5 6 9 
+7 8 1
+
+3 
+3
+1 2 3
+-2 3 -9
+6 5 8
+
+
+3
+2.8 -3.6 4.5
+6.5 7.45 8.09
+-9.3 6.34 5.55
+
+3
+2 -3 4
+6 7 8
+-9 6 5
+
+3
+-2 3 -4
+-6 -7 -8
+9 -6 -5
+*/
